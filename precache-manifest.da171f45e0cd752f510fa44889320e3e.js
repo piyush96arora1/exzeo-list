ffe96ddf2257808a5175456c940802e29ddb16c2
unknown <piyush96arora@gmail.com>
2019-06-12 01:51:22 +0530

    Updates

---
+++ precache-manifest.da171f45e0cd752f510fa44889320e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5102a335572427b65121e0b943c5cab0",
+    "url": "/exzeo-list/index.html"
+  },
+  {
+    "revision": "f17be07e53ce8de85010",
+    "url": "/exzeo-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8e63a6e174cc6b115b12",
+    "url": "/exzeo-list/static/js/2.2b9aa225.chunk.js"
+  },
+  {
+    "revision": "f17be07e53ce8de85010",
+    "url": "/exzeo-list/static/js/main.fa6edc52.chunk.js"
+  },
+  {
+    "revision": "3f8525e879617cfc0fbb",
+    "url": "/exzeo-list/static/js/runtime~main.7877fd7c.js"
+  }
+]);
